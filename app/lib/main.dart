2fa12d58467224fd1adb9a254d755566010d3ce6
Miguel Ramirez <miguel.ramirez@mangoapp.com.py>
2026-09-24 13:20:01 -0300

    fix: correct history screen import path to relative path

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:trading_bot/screens/history/history_screen.dart';
+import 'screens/history/history_screen.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:toastification/toastification.dart';
 import 'package:flutter/material.dart';
